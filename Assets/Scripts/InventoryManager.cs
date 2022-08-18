d7bdc77210b509439db44893b440ba98bf3ca97f
alphaAE <a1226123914@gmail.com>
2022-08-18 13:03:17 +0800

    增加 物品Json解析机制

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour {
@@ -18,6 +19,18 @@ public class InventoryManager : MonoBehaviour {
 
     private void ParserItemJson() {
         TextAsset itemText = Resources.Load<TextAsset>("items");
-        Debug.Log(itemText.text);
+        JArray jArray = JArray.Parse(itemText.text);
+
+        foreach (var jToken in jArray) {
+            var typeStr = jToken.Value<String>("type");
+            if (typeStr is null) continue;
+            Type type = Type.GetType(typeStr);
+            if (type is null) continue;
+            Item item = (Item)jToken.ToObject(type);
+
+            if (item is not null) {
+                Debug.Log(item.ToString());
+            }
+        }
     }
 }
