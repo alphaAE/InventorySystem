3fd8c2ef16059896d60628518339d0741db367a0
alphaAE <a1226123914@gmail.com>
2022-08-24 17:55:17 +0800

    修复 ToolTip超出屏幕问题

--- Assets/Scripts/UI/ToolTipUI.cs
+++ Assets/Scripts/UI/ToolTipUI.cs
@@ -9,6 +9,7 @@ public class ToolTipUI : MonoBehaviour {
     public static ToolTipUI Instance { get; private set; }
 
     private RectTransform _canvasRect;
+    private RectTransform _rect;
     private Text _toolTipText, _contentText;
     private CanvasGroup _canvasGroup;
 
@@ -20,6 +21,7 @@ public class ToolTipUI : MonoBehaviour {
     private void Awake() {
         Instance = this;
         _canvasRect = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        _rect = GetComponent<RectTransform>();
         _toolTipText = GetComponent<Text>();
         _contentText = transform.Find("text").GetComponent<Text>();
         _canvasGroup = GetComponent<CanvasGroup>();
@@ -27,11 +29,31 @@ public class ToolTipUI : MonoBehaviour {
 
     private void Update() {
         if (IsDisplay) {
+            ResetPivot();
             Vector2 position;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvasRect, Input.mousePosition, null,
                 out position);
             transform.localPosition = position + _toolTipPositionOffset;
         }
+    }
+
+    private void ResetPivot() {
+        // 计算轴心
+        Vector2 position;
+        float xOffset = 0, yOffset = 1;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvasRect, Input.mousePosition, null,
+            out position);
+        if (position.x + _canvasRect.rect.width / 2 + _rect.rect.width > _canvasRect.rect.width) {
+            print("宽度超出");
+            xOffset = 1;
+        }
+
+        if (Mathf.Abs(position.y - _canvasRect.rect.height / 2) + _rect.rect.height > _canvasRect.rect.height) {
+            print("高度超出");
+            yOffset = 0;
+        }
+
+        _rect.pivot = new Vector2(xOffset, yOffset);
     }
 
     public void Show(string content) {
